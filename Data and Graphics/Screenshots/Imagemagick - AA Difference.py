7e4a7ab051ab68444c41c78bb76f64e4fd07117e
GuestJim <30958420+GuestJim@users.noreply.github.com>
2019-02-22 21:38:52 -0500

    Create Imagemagick - AA Difference.py

---
+++ Data and Graphics/Screenshots/Imagemagick - AA Difference.py
@@ -0,0 +1,37 @@
+import sys,os
+
+droppedPath = sys.argv[1].rsplit("\\",1)[0] + "\\"
+
+#droppedFiles = sys.argv[1:]
+#	works for dropping all relevant files onto it, but below is better
+droppedFiles = os.listdir(droppedPath)
+#	with this one file dropped from the folder is enough
+os.chdir(droppedPath)
+#	this is necessary for the resulting file names to be correct
+
+if not os.path.exists(droppedPath + "Difference"):
+	os.mkdir(droppedPath + "Difference")
+
+for file in droppedFiles:
+	if "None" in file and "MLAA" not in file:
+		droppedNone00 = file
+
+droppedFiles.remove(droppedNone00)
+
+for drop in droppedFiles:
+	if ".png" in drop and "MLAA" not in drop:
+		os.system("convert \"" + droppedNone00 + "\" \"" + drop + "\" -compose difference -composite \"" + droppedPath + "Difference\\Difference " + drop + "\"")
+
+BASE = []
+MLAA = []
+
+for IMAG in os.listdir(droppedPath):
+	if "MLAA" in IMAG:
+		MLAA.append(IMAG)
+	else:
+		BASE.append(IMAG)
+
+for POS in range(0, len(MLAA)):
+	os.system("convert \"" + BASE[POS] + "\" \"" + MLAA[POS] + "\" -compose difference -composite \"" + droppedPath + "Difference\\MLAA Difference " + BASE[POS] + "\"")
+
+#os.system("pause")
